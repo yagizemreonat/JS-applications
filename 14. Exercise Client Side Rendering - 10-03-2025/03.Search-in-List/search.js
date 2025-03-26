70c359c366add73a2d18876e9f55137be932250b
YagizONAT <yagizemreonat@gmail.com>
2025-03-26 22:49:32 +0200

    Commit

--- 14. Exercise Client Side Rendering - 10-03-2025/03.Search-in-List/search.js
+++ 14. Exercise Client Side Rendering - 10-03-2025/03.Search-in-List/search.js
@@ -1,3 +1,45 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+
+import { towns } from './towns.js';
+console.log(towns);
+
+const townsDivEl = document.getElementById('towns');
+const searchInputEl = document.getElementById('searchText');
+const searchBtnEl = document.getElementById('searchBtn');
+const resultDivEl = document.getElementById('result');
+
+searchBtnEl.addEventListener('click', search);
+
+function showTowns() {
+    render(townsTemplate(), townsDivEl);
+}
+
+showTowns();
+
+function townsTemplate() {
+    return html`
+        <ul>
+            ${towns.map(town => html`<li>${town}</li>`)}
+        </ul>
+    `;
+}
+
+
+
 function search() {
-   // TODO
+   const searchedValue = searchInputEl.value;
+   console.log(searchedValue);
+   
+   const allLiEls = Array.from(document.querySelectorAll('li'));
+   let matchCount = 0;
+
+   allLiEls.forEach(liEl => {
+      if (liEl.textContent.includes(searchedValue)) {
+         liEl.className = "active";
+         matchCount++;
+      } else {
+         liEl.className = "";
+      }
+   });    
+   resultDivEl.textContent = `${matchCount} matches found`;
 }
--- 14. Exercise Client Side Rendering - 10-03-2025/03.Search-in-List/towns.js
+++ 14. Exercise Client Side Rendering - 10-03-2025/03.Search-in-List/towns.js
@@ -6,3 +6,4 @@ export const towns = [
     'Dolna Bania',
     'Gorna Bania'
 ];
+
